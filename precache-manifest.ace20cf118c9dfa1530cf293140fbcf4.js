089a296817b8c7a59f7812442210098e9a482101
Denis Shilov <schden14@mail.ru>
2019-04-21 22:12:30 +0300

    Updates

---
+++ precache-manifest.ace20cf118c9dfa1530cf293140fbcf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8933784b5ae60e133808",
+    "url": "/static/css/main.06ba2bc2.chunk.css"
+  },
+  {
+    "revision": "8933784b5ae60e133808",
+    "url": "/static/js/main.9492f557.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ee8b0f9bf970415fd10",
+    "url": "/static/js/2.52d9ac87.chunk.js"
+  },
+  {
+    "revision": "0fe1e8f01cc19c50a3b7ab48f8f1857b",
+    "url": "/index.html"
+  }
+];
